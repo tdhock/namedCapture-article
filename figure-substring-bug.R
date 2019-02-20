97b0445a1fdb43cd6dcdc0785aeab0a92190c9a3
Toby Dylan Hocking <tdhock5@gmail.com>
2019-02-20 11:51:23 -0700

    add stringi

--- figure-substring-bug.R
+++ figure-substring-bug.R
@@ -1,12 +1,15 @@
 N.vec <- 2^seq(4, 10)
 library(data.table)
+library(stringi)
 timing.dt.list <- list()
 for(N in N.vec){
   subject.vec <- rep("A", l=N)
   subject <- paste(subject.vec, collapse="")
   start.vec <- end.vec <- 1:N #slow too.
   start.vec <- end.vec <- rep(1, N)#slow.
-  timing <- microbenchmark::microbenchmark(substring={
+  timing <- microbenchmark::microbenchmark("stringi::stri_sub"={
+    stringi::stri_sub(subject, start.vec, end.vec)
+  }, "base::substring"={
     substring(subject, start.vec, end.vec)
   }, times=5)
   print(timing)
@@ -18,22 +21,25 @@ timing.dt[, ms := time/1e6]
   median=median(ms),
   q25=quantile(ms, 0.25),
   q75=quantile(ms, 0.75)
-  ), by=list(N)])
+  ), by=list(expr, N)])
+
 library(ggplot2)
 gg <- ggplot()+
   ggtitle(
     "Bug: substring in R is quadratic but should be linear time complexity")+
   geom_ribbon(aes(
-    N, ymin=q25, ymax=q75),
+    N, ymin=q25, ymax=q75, fill=expr),
     alpha=0.5,
     data=stats.dt)+
   geom_line(aes(
-    N, median),
+    N, median, color=expr),
     data=stats.dt)+
   scale_x_continuous(
-    "N = number of substrings = nchar(text)")+
+    "N = number of substrings = nchar(text)",
+    limits=c(NA, 1400))+
   scale_y_continuous(
     "milliseconds (median line and quartile bands)")
+dl <- directlabels::direct.label(gg, "last.polygons")
 png("figure-substring-bug.png")
-print(gg)
+print(dl)
 dev.off()
