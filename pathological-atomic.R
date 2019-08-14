aabd6f685bd884dbdf107a64b4228e424d1418a7
Toby Dylan Hocking <tdhock5@gmail.com>
2019-08-14 00:44:42 -0700

    reviews

--- pathological-atomic.R
+++ pathological-atomic.R
@@ -3,16 +3,13 @@ times.list <- list()
 for(N in 1:max.N){
   cat(sprintf("subject/pattern size %4d / %4d\n", N, max.N))
   subject <- paste(rep("a", N), collapse="")
-  pattern <- paste(rep(c("a?", "a"), each=N), collapse="")
+  pattern <- paste(rep(c("(?>a?)", "a"), each=N), collapse="")
   N.times <- microbenchmark::microbenchmark(
     ICU={
       stringi::stri_match(subject, regex=pattern)
     },
     PCRE={
       regexpr(pattern, subject, perl=TRUE)
-    },
-    TRE={
-      regexpr(pattern, subject, perl=FALSE)
     },
     RE2={
       re2r::re2_match(pattern, subject)
